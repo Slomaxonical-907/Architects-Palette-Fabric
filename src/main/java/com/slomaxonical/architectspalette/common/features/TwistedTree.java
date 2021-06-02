137bf1364305e3414f8beb57199ab0a0d4fb52fb
Slomaxonical-907 <Slomaxonical907@gmail.com>
2021-06-02 23:27:01 +0300

    twisted leaves?

--- src/main/java/com/slomaxonical/architectspalette/common/features/TwistedTree.java
+++ src/main/java/com/slomaxonical/architectspalette/common/features/TwistedTree.java
@@ -3,6 +3,7 @@ package com.slomaxonical.architectspalette.common.features;
 import com.slomaxonical.architectspalette.core.registry.APBlocks;
 import net.minecraft.block.sapling.SaplingGenerator;
 import net.minecraft.util.Lazy;
+import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
@@ -23,7 +24,7 @@ public class TwistedTree extends SaplingGenerator {
                     new ForkingTrunkPlacer(5, 2, 2),
                     new SimpleBlockStateProvider(APBlocks.TWISTED_LEAVES.getDefaultState()),
                     new SimpleBlockStateProvider(APBlocks.TWISTED_SAPLING.getDefaultState()),
-                    new AcaciaFoliagePlacer(UniformIntProvider.create(2,3), UniformIntProvider.create(0,1)),
+                    new AcaciaFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0)),
                     new TwoLayersFeatureSize(1, 0, 2)
             )
                     .ignoreVines()
