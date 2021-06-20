d0a1f16ab514904039312ad490709370e4606efd
Slomaxonical-907 <Slomaxonical907@gmail.com>
2021-06-20 22:15:40 +0300

    1.2

--- src/main/java/com/slomaxonical/architectspalette/compat/rei/APPlugin.java
+++ src/main/java/com/slomaxonical/architectspalette/compat/rei/APPlugin.java
@@ -1,18 +1,10 @@
 package com.slomaxonical.architectspalette.compat.rei;
 
 import com.slomaxonical.architectspalette.core.crafting.WarpingRecipe;
-import com.slomaxonical.architectspalette.core.registry.APItems;
-import dev.architectury.event.EventResult;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
-import me.shedaniel.rei.api.client.registry.entry.EntryRegistry;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-import me.shedaniel.rei.api.common.util.EntryStacks;
-import net.minecraft.item.ItemStack;
-
-import java.util.List;
-import java.util.Objects;
 
 public class APPlugin implements REIClientPlugin {
     public static final CategoryIdentifier<WarpingDisplay> WARPING = CategoryIdentifier.of("architects_palette", "portal_warping");
