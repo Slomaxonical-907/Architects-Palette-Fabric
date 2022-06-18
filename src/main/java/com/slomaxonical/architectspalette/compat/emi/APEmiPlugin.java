f8399e43034c50ff1f62928fa505b5adee578f9c
Kat <Kat@sleeping.town>
2022-06-18 14:32:08 -0400

    Convert to spaces for consistency
    Sorry lol I forgor

--- src/main/java/com/slomaxonical/architectspalette/compat/emi/APEmiPlugin.java
+++ src/main/java/com/slomaxonical/architectspalette/compat/emi/APEmiPlugin.java
@@ -10,18 +10,18 @@ import dev.emi.emi.api.stack.EmiStack;
 import net.minecraft.util.Identifier;
 
 public class APEmiPlugin implements EmiPlugin {
-	public static EmiRecipeCategory WARPING = new EmiRecipeCategory(new Identifier("architects_palette:warping"),
-			EmiStack.of(APBlocks.WARPSTONE));
-	
-	@Override
-	public void register(EmiRegistry registry) {
-		registry.addCategory(WARPING);
-		
-		registry.addWorkstation(WARPING, EmiStack.of(APBlocks.WARPSTONE));
-		
-		for(WarpingRecipe recipe : registry.getRecipeManager().listAllOfType(WarpingRecipe.TYPE)) {
-			registry.addRecipe(new WarpingEmiRecipe(recipe));
-		}
-	}
+    public static EmiRecipeCategory WARPING = new EmiRecipeCategory(new Identifier("architects_palette:warping"),
+            EmiStack.of(APBlocks.WARPSTONE));
+    
+    @Override
+    public void register(EmiRegistry registry) {
+        registry.addCategory(WARPING);
+        
+        registry.addWorkstation(WARPING, EmiStack.of(APBlocks.WARPSTONE));
+        
+        for(WarpingRecipe recipe : registry.getRecipeManager().listAllOfType(WarpingRecipe.TYPE)) {
+            registry.addRecipe(new WarpingEmiRecipe(recipe));
+        }
+    }
 
 }
--- src/main/java/com/slomaxonical/architectspalette/compat/emi/WarpingEmiRecipe.java
+++ src/main/java/com/slomaxonical/architectspalette/compat/emi/WarpingEmiRecipe.java
@@ -15,57 +15,57 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 
 public class WarpingEmiRecipe implements EmiRecipe{
-	private final Identifier id;
-	private final EmiIngredient input;
-	private final EmiStack output;
-	
-	public WarpingEmiRecipe(WarpingRecipe recipe) {
-		this.id = recipe.getId();
-		input = EmiIngredient.of(recipe.getInput());
-		output = EmiStack.of(recipe.getOutput());
-	}
-	
-	@Override
-	public EmiRecipeCategory getCategory() {
-		return APEmiPlugin.WARPING;
-	}
-	
-	@Override
-	public List<EmiIngredient> getInputs() {
-		return List.of(input);
-	}
-	
-	@Override
-	public List<EmiStack> getOutputs() {
-		return List.of(output);
-	}
-	
-	@Override
-	public Identifier getId() {
-		return id;
-	}
-	
-	@Override
-	public int getDisplayHeight() {
-		return 52;
-	}
-	
-	@Override
-	public int getDisplayWidth() {
-		return 142;
-	}
-	
-	@Override
-	public void addWidgets(WidgetHolder widgets) {
-		widgets.addTexture(new Identifier("architects_palette", "textures/gui/nether_portal.png"), (widgets.getWidth() / 2) - 32/2, widgets.getHeight()/2-14, 32, 32, 0, 0, 32, 32, 32, 32);
-		widgets.addSlot(input, widgets.getWidth()/2-45, 21);
-		widgets.addSlot(output, widgets.getWidth()/2+30, 21).recipeContext(this);
-		
-		widgets.addText(new LiteralText("toss").asOrderedText(), widgets.getWidth()/2-13, 2, 0xffffff, true);
-		//TextWidget doesn't have a tooltip method, so we add a no-op Drawable and add the tooltip to that instead.
-		//Might as well make the widget cover the Portal texture too while we're here.
-		TranslatableText dimensionName = new TranslatableText("architects_palette.info.warping_toss_description","the Nether");
-		widgets.addDrawable(widgets.getWidth()/2-18, 2, 36, 44, (matrices, mouseX, mouseY, delta) -> {})
-			.tooltip((mouseX, mouseY) -> List.of(TooltipComponent.of(new LiteralText(dimensionName.getString()).asOrderedText())));
-	}
+    private final Identifier id;
+    private final EmiIngredient input;
+    private final EmiStack output;
+    
+    public WarpingEmiRecipe(WarpingRecipe recipe) {
+        this.id = recipe.getId();
+        input = EmiIngredient.of(recipe.getInput());
+        output = EmiStack.of(recipe.getOutput());
+    }
+    
+    @Override
+    public EmiRecipeCategory getCategory() {
+        return APEmiPlugin.WARPING;
+    }
+    
+    @Override
+    public List<EmiIngredient> getInputs() {
+        return List.of(input);
+    }
+    
+    @Override
+    public List<EmiStack> getOutputs() {
+        return List.of(output);
+    }
+    
+    @Override
+    public Identifier getId() {
+        return id;
+    }
+    
+    @Override
+    public int getDisplayHeight() {
+        return 52;
+    }
+    
+    @Override
+    public int getDisplayWidth() {
+        return 142;
+    }
+    
+    @Override
+    public void addWidgets(WidgetHolder widgets) {
+        widgets.addTexture(new Identifier("architects_palette", "textures/gui/nether_portal.png"), (widgets.getWidth() / 2) - 32/2, widgets.getHeight()/2-14, 32, 32, 0, 0, 32, 32, 32, 32);
+        widgets.addSlot(input, widgets.getWidth()/2-45, 21);
+        widgets.addSlot(output, widgets.getWidth()/2+30, 21).recipeContext(this);
+        
+        widgets.addText(new LiteralText("toss").asOrderedText(), widgets.getWidth()/2-13, 2, 0xffffff, true);
+        //TextWidget doesn't have a tooltip method, so we add a no-op Drawable and add the tooltip to that instead.
+        //Might as well make the widget cover the Portal texture too while we're here.
+        TranslatableText dimensionName = new TranslatableText("architects_palette.info.warping_toss_description","the Nether");
+        widgets.addDrawable(widgets.getWidth()/2-18, 2, 36, 44, (matrices, mouseX, mouseY, delta) -> {})
+            .tooltip((mouseX, mouseY) -> List.of(TooltipComponent.of(new LiteralText(dimensionName.getString()).asOrderedText())));
+    }
 }
